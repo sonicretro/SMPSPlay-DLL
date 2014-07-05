a8755a57f0a7f83486ebf7877b8ab0a749f22d34
MainMemory <sonicmike2@yahoo.com>
2014-07-05 12:23:05 -0500

    Adding configuration tool.

---
+++ SMPSCFG/MainForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace SMPSCFG
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.setAllToDefaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.setAllToMIDIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.setAllToRandomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 24);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(584, 538);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.AutoScroll = true;
+			this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(576, 512);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "All";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.AutoScroll = true;
+			this.tabPage2.Controls.Add(this.tableLayoutPanel2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(576, 536);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Sonic 3 & Knuckles";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.AutoScroll = true;
+			this.tabPage3.Controls.Add(this.tableLayoutPanel3);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(576, 536);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Sonic 3";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.AutoScroll = true;
+			this.tabPage4.Controls.Add(this.tableLayoutPanel4);
+			this.tabPage4.Location = new System.Drawing.Point(4, 22);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage4.Size = new System.Drawing.Size(576, 536);
+			this.tabPage4.TabIndex = 3;
+			this.tabPage4.Text = "Sonic & Knuckles";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(0, 0);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.AutoSize = true;
+			this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 6);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(0, 0);
+			this.tableLayoutPanel2.TabIndex = 1;
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.AutoSize = true;
+			this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(6, 6);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(0, 0);
+			this.tableLayoutPanel3.TabIndex = 1;
+			// 
+			// tableLayoutPanel4
+			// 
+			this.tableLayoutPanel4.AutoSize = true;
+			this.tableLayoutPanel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel4.ColumnCount = 2;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(6, 6);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.RowCount = 1;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(0, 0);
+			this.tableLayoutPanel4.TabIndex = 1;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// editToolStripMenuItem
+			// 
+			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setAllToDefaultToolStripMenuItem,
+            this.setAllToMIDIToolStripMenuItem,
+            this.setAllToRandomToolStripMenuItem});
+			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+			this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+			this.editToolStripMenuItem.Text = "&Edit";
+			// 
+			// setAllToDefaultToolStripMenuItem
+			// 
+			this.setAllToDefaultToolStripMenuItem.Name = "setAllToDefaultToolStripMenuItem";
+			this.setAllToDefaultToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+			this.setAllToDefaultToolStripMenuItem.Text = "Set All to &Default";
+			this.setAllToDefaultToolStripMenuItem.Click += new System.EventHandler(this.setAllToDefaultToolStripMenuItem_Click);
+			// 
+			// setAllToMIDIToolStripMenuItem
+			// 
+			this.setAllToMIDIToolStripMenuItem.Name = "setAllToMIDIToolStripMenuItem";
+			this.setAllToMIDIToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+			this.setAllToMIDIToolStripMenuItem.Text = "Set All to &MIDI";
+			this.setAllToMIDIToolStripMenuItem.Click += new System.EventHandler(this.setAllToMIDIToolStripMenuItem_Click);
+			// 
+			// setAllToRandomToolStripMenuItem
+			// 
+			this.setAllToRandomToolStripMenuItem.Name = "setAllToRandomToolStripMenuItem";
+			this.setAllToRandomToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+			this.setAllToRandomToolStripMenuItem.Text = "Set All to &Random";
+			this.setAllToRandomToolStripMenuItem.Click += new System.EventHandler(this.setAllToRandomToolStripMenuItem_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 562);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "SMPS Player Configuration";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
+			this.tabPage4.ResumeLayout(false);
+			this.tabPage4.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem setAllToDefaultToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem setAllToMIDIToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem setAllToRandomToolStripMenuItem;
+	}
+}
+
---
+++ SMPSCFG/MainForm.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SMPSCFG
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		static readonly string[] TrackKeys = {
+			"TitleScreenTrack",
+			"AngelIsland1Track",
+			"AngelIsland2Track",
+			"Hydrocity1Track",
+			"Hydrocity2Track",
+			"MarbleGarden1Track",
+			"MarbleGarden2Track",
+			"CarnivalNight1Track",
+			"CarnivalNight2Track",
+			"FlyingBattery1Track",
+			"FlyingBattery2Track",
+			"IceCap1Track",
+			"IceCap2Track",
+			"LaunchBase1Track",
+			"LaunchBase2Track",
+			"MushroomHill1Track",
+			"MushroomHill2Track",
+			"Sandopolis1Track",
+			"Sandopolis2Track",
+			"LavaReef1Track",
+			"LavaReef2Track",
+			"SkySanctuaryTrack",
+			"DeathEgg1Track",
+			"DeathEgg2Track",
+			"MidbossTrack",
+			"BossTrack",
+			"DoomsdayTrack",
+			"MagneticOrbsTrack",
+			"SpecialStageTrack",
+			"SlotMachineTrack",
+			"GumballMachineTrack",
+			"KnucklesTrack",
+			"AzureLakeTrack",
+			"BalloonParkTrack",
+			"DesertPalaceTrack",
+			"ChromeGadgetTrack",
+			"EndlessMineTrack",
+			"GameOverTrack",
+			"ContinueTrack",
+			"ActClearTrack",
+			"1UpTrack",
+			"ChaosEmeraldTrack",
+			"InvincibilityTrack",
+			"CompetitionMenuTrack",
+			"UnusedTrack",
+			"LevelSelectTrack",
+			"FinalBossTrack",
+			"DrowningTrack",
+			"AllClearTrack",
+			"CreditsTrack"
+		};
+
+		static readonly string[] TrackNames = {
+			"Title Screen",
+			"Angel Island Zone Act 1",
+			"Angel Island Zone Act 2",
+			"Hydrocity Zone Act 1",
+			"Hydrocity Zone Act 2",
+			"Marble Garden Zone Act 1",
+			"Marble Garden Zone Act 2",
+			"Carnival Night Zone Act 1",
+			"Carnival Night Zone Act 2",
+			"Flying Battery Zone Act 1",
+			"Flying Battery Zone Act 2",
+			"IceCap Zone Act 1",
+			"IceCap Zone Act 2",
+			"Launch Base Zone Act 1",
+			"Launch Base Zone Act 2",
+			"Mushroom Hill Zone Act 1",
+			"Mushroom Hill Zone Act 2",
+			"Sandopolis Zone Act 1",
+			"Sandopolis Zone Act 2",
+			"Lava Reef Zone Act 1",
+			"Lava Reef Zone Act 2",
+			"Sky Sanctuary Zone",
+			"Death Egg Zone Act 1",
+			"Death Egg Zone Act 2",
+			"Midboss",
+			"Boss",
+			"The Doomsday Zone",
+			"Magnetic Orbs",
+			"Special Stage",
+			"Slot Machine",
+			"Gumball Machine",
+			"Knuckles' Theme",
+			"Azure Lake",
+			"Balloon Park",
+			"Desert Palace",
+			"Chrome Gadget",
+			"Endless Mine",
+			"Game Over",
+			"Continue",
+			"Act Clear",
+			"1-Up",
+			"Chaos Emerald",
+			"Invincibility",
+			"Competition Menu",
+			"Unused",
+			"Level Select",
+			"Final Boss",
+			"Drowning",
+			"All Clear",
+			"Credits"
+		};
+
+		static readonly string[] TitleScreenTrackOptions = { "S3", "S&K", "S&K0525" };
+
+		static readonly string[] MidbossTrackOptions = { "S3", "S&K" };
+
+		static readonly string[] KnucklesTrackOptions = { "S3", "S&K", "PC" };
+
+		static readonly string[] OneUpTrackOptions = { "S3", "S&K" };
+
+		static readonly string[] InvincibilityTrackOptions = { "S3", "S&K", "S3PC", "PC" };
+
+		static readonly string[] AllClearTrackOptions = { "S3", "S&K", "S&K0525" };
+
+		static readonly string[] CreditsTrackOptions = { "S3", "S&K", "S3C", "S&K0525", "PC" };
+
+		static readonly string[] ContinueTrackOptions = { "S3", "S&K" };
+
+		static readonly string[] CarnivalNight1TrackOptions = { "MD", "PC" };
+
+		static readonly string[] CarnivalNight2TrackOptions = { "MD", "PC" };
+
+		static readonly string[] IceCap1TrackOptions = { "MD", "PC" };
+
+		static readonly string[] IceCap2TrackOptions = { "MD", "PC" };
+
+		static readonly string[] LaunchBase1TrackOptions = { "MD", "PC" };
+
+		static readonly string[] LaunchBase2TrackOptions = { "MD", "PC" };
+
+		static readonly string[] CompetitionMenuTrackOptions = { "MD", "PC" };
+
+		static readonly string[][] TrackOptions = {
+			TitleScreenTrackOptions,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			CarnivalNight1TrackOptions,
+			CarnivalNight2TrackOptions,
+			null,
+			null,
+			IceCap1TrackOptions,
+			IceCap2TrackOptions,
+			LaunchBase1TrackOptions,
+			LaunchBase2TrackOptions,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			MidbossTrackOptions,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			KnucklesTrackOptions,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			ContinueTrackOptions,
+			null,
+			OneUpTrackOptions,
+			null,
+			InvincibilityTrackOptions,
+			CompetitionMenuTrackOptions,
+			null,
+			null,
+			null,
+			null,
+			AllClearTrackOptions,
+			CreditsTrackOptions
+		};
+
+		static readonly string[] GameIDs = { string.Empty, "S3K", "S3", "S&K" };
+
+		Dictionary<string, Dictionary<string, string>> INI;
+
+		static readonly List<ComboBox>[] comboboxes = new List<ComboBox>[4];
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			TableLayoutPanel[] tables = { tableLayoutPanel1, tableLayoutPanel2, tableLayoutPanel3, tableLayoutPanel4 };
+			if (File.Exists("SMPSOUT.ini"))
+				INI = IniFile.IniFile.Load("SMPSOUT.ini");
+			else
+				INI = new Dictionary<string, Dictionary<string, string>>();
+			for (int gn = 0; gn < GameIDs.Length; gn++)
+			{
+				string game = GameIDs[gn];
+				if (!INI.ContainsKey(game))
+					INI.Add(game, new Dictionary<string, string>());
+				Dictionary<string, string> group = INI[game];
+				TableLayoutPanel table = tables[gn];
+				table.SuspendLayout();
+				comboboxes[gn] = new List<ComboBox>();
+				for (int tn = 0; tn < TrackKeys.Length; tn++)
+				{
+					string track = TrackKeys[tn];
+					table.Controls.Add(new Label() { Text = TrackNames[tn] + ":", AutoSize = true, TextAlign = ContentAlignment.MiddleLeft }, 0, tn);
+					ComboBox cb = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+					cb.Items.Add("Default");
+					cb.Items.Add("MIDI");
+					string[] opts = TrackOptions[tn];
+					if (opts != null)
+					{
+						cb.Items.Add("Random");
+						cb.Items.AddRange(opts);
+					}
+					table.Controls.Add(cb, 1, tn);
+					comboboxes[gn].Add(cb);
+					if (!group.ContainsKey(track))
+					{
+						group.Add(track, "Default");
+						cb.SelectedIndex = 0;
+					}
+					else
+						switch ((group[track] ?? "default").ToLowerInvariant())
+						{
+							case "default":
+								cb.SelectedIndex = 0;
+								break;
+							case "midi":
+								cb.SelectedIndex = 1;
+								break;
+							case "random":
+								cb.SelectedIndex = opts != null ? 2 : 0;
+								break;
+							default:
+								cb.SelectedIndex = 0;
+								if (opts != null)
+									for (int on = 0; on < opts.Length; on++)
+										if (group[track].Equals(opts[on], StringComparison.OrdinalIgnoreCase))
+										{
+											cb.SelectedIndex = on + 3;
+											break;
+										}
+								break;
+						}
+					if (tn > 0)
+						table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+					if (opts == null)
+						cb.SelectedIndexChanged += new EventHandler((s, ev) =>
+						{
+							string str;
+							if (cb.SelectedIndex == 1)
+								str = "MIDI";
+							else
+								str = "Default";
+							INI[game][track] = str;
+						});
+					else
+						cb.SelectedIndexChanged += new EventHandler((s, ev) =>
+						{
+							string str;
+							if (cb.SelectedIndex > 2)
+								str = opts[cb.SelectedIndex - 3];
+							else if (cb.SelectedIndex == 2)
+								str = "Random";
+							else if (cb.SelectedIndex == 1)
+								str = "MIDI";
+							else
+								str = "Default";
+							INI[game][track] = str;
+						});
+				}
+				table.ResumeLayout();
+			}
+		}
+
+		private void setAllToDefaultToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach (ComboBox cb in comboboxes[tabControl1.SelectedIndex])
+				cb.SelectedIndex = 0;
+		}
+
+		private void setAllToMIDIToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach (ComboBox cb in comboboxes[tabControl1.SelectedIndex])
+				cb.SelectedIndex = 1;
+		}
+
+		private void setAllToRandomToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach (ComboBox cb in comboboxes[tabControl1.SelectedIndex].Where(cb => cb.Items.Count > 2))
+				cb.SelectedIndex = 2;
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			foreach (string game in GameIDs)
+			{
+				Dictionary<string, string> group = INI[game];
+				foreach (string track in TrackKeys)
+					if (group[track].Equals("Default", StringComparison.OrdinalIgnoreCase))
+						group.Remove(track);
+				if (group.Count == 0)
+					INI.Remove(game);
+			}
+			IniFile.IniFile.Save(INI, "SMPSOUT.ini");
+		}
+	}
+}
